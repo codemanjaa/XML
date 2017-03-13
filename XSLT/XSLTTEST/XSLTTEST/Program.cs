63a50eb25903bbd08df5d089aa495d8d17b3c90a
Codemanja <codemanjaa@gmail.com>
2017-03-13 12:48:18 +0200

    XSLT Testing completed version

---
+++ XSLT/XSLTTEST/XSLTTEST/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace XSLTTEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XslTransform myxslTransform;
+            myxslTransform = new XslTransform();
+            myxslTransform.Load("TeacherTest.xslt");
+            myxslTransform.Transform("Teachers.xml", "output1.html");
+        }
+    }
+}
